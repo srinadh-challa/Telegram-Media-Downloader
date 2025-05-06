ae3e1ec7847c51cbb443dc2c8335f38c24458979
srinadh <srinadhchallaa@gmail.com>
2025-05-06 15:58:53 +0530

    chnages

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-# Install yt-dlp globally
-pip install yt-dlp
+# Install Python and pip
+apt-get update && apt-get install -y python3 python3-pip
+
+# Install yt-dlp globally for Python 3
+pip3 install yt-dlp
+
+# Proceed with Node dependencies
+npm install
